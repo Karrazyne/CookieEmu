2432d941fb8cda6b555d6a14bcbf16d69ceb0e31
Karrazyne <karradev30@gmail.com>
2017-09-14 11:12:34 +0200

    c

---
+++ CookieEmu.API/Protocol/Messages/Custom/ClearIdentificationMessage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CookieEmu.API.IO;
+
+namespace CookieEmu.API.Protocol.Messages.Custom
+{
+    public class ClearIdentificationMessage : NetworkMessage
+    {
+        public override ushort MessageID => ProtocolId;
+        public const ushort ProtocolId = 888;
+
+        public bool AutoConnect { get; set; }
+        public string Lang { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public short ServerId { get; set; }
+        public string ServerIp { get; set; }
+        public string HardwareId { get; set; }
+
+        public override void Serialize(IDataWriter writer)
+        {
+
+        }
+
+        public override void Deserialize(IDataReader reader)
+        {
+            var loc2 = reader.ReadByte();
+            AutoConnect = BooleanByteWrapper.GetFlag(loc2, 0);
+            Lang = reader.ReadUTF();
+            Username = reader.ReadUTF();
+            Password = reader.ReadUTF();
+            ServerId = reader.ReadShort();
+            ServerIp = reader.ReadUTF();
+            HardwareId = reader.ReadUTF();
+        }
+    }
+}
